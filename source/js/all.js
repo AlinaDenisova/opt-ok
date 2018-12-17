3bc85f194c6d21d7bbc1170b4f203dff9bba0212
AlinaDenisova <denicov_vasya@mail.ru>
2018-12-17 22:55:13 +0400

    Правки слайдера

--- source/js/all.js
+++ source/js/all.js
@@ -210,169 +210,72 @@ window.filterLeft = (function () {
   }
 })();
 
-'use strict';
-
-(function () {
-    var promo = document.querySelector('.promo');
-    var novelties = document.querySelector('.novelties-of-week');
-    var discount = document.querySelector('.discount');
-
-    if (promo && discount && novelties) {
-      var promoSlides = promo.querySelectorAll('.promo__slide');
-      var activePromoSlideIndex = [].indexOf.call(promoSlides, promo.querySelector('.promo__slide--active'));
-      var noveltiesSlides = novelties.querySelectorAll('.novelties-of-week__slide');
-      var activeNoveltiesSlideIndex = [].indexOf.call(noveltiesSlides, novelties.querySelector('.novelties-of-week__slide--active'));
-      var discountSlides = discount.querySelectorAll('.discount__slide');
-      var activeDiscountSlideIndex = [].indexOf.call(discountSlides, discount.querySelector('.discount__slide--active'));
-      var dots = null;
-      var dotsContainer = promo.querySelector('.slider__dots');
-      var btnPromoPrev = promo.querySelector('.promo__btn--prev');
-      var btnPromoNext = promo.querySelector('.promo__btn--next');
-      var btnNoveltiesPrev = novelties.querySelector('.novelties-of-week__btn--prev');
-      var btnNoveltiesNext = novelties.querySelector('.novelties-of-week__btn--next');
-      var btnDiscountPrev = discount.querySelector('.discount__btn--prev');
-      var btnDiscountNext = discount.querySelector('.discount__btn--next');
-      var PREV = 'prev';
-      var NEXT = 'next';
-
-    var renderDots = function () {
-      var dot = null;
-      for (var i = 0; i < promoSlides.length; i++) {
-        dot = document.createElement('li');
-        dot.classList.add('slider__dot');
-        dotsContainer.appendChild(dot);
+$(function() {
+  $('.promo__slides').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    dots: false,
+    navContainerClass: ['slider-nav', 'slider-nav--promo'],
+    navClass: ['promo__btn promo__btn--prev', 'promo__btn promo__btn--next'],
+    responsive: {
+      1140: {
+        dots: true,
+        dotsClass: ['slider__dots'],
+        dotClass: ['slider__dot slider__dot--promo']
       }
+    }
+  });
 
-      dots = dotsContainer.querySelectorAll('.slider__dot');
-      dots[activePromoSlideIndex].classList.add('slider__dot--active');
-    };
+  $('.novelties-of-week__slides').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navContainerClass: ['slider-nav', 'slider-nav--novelties-of-week'],
+    navClass: ['novelties-of-week__btn novelties-of-week__btn--prev', 'novelties-of-week__btn novelties-of-week__btn--next']
+  });
 
-    var activatePromoSlide = function () {
-      promoSlides[activePromoSlideIndex].classList.add('promo__slide--active');
-      dots[activePromoSlideIndex].classList.add('slider__dot--active');
-    };
+  $('.discount__slides').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navContainerClass: ['slider-nav', 'slider-nav--discount'],
+    navClass: ['discount__btn discount__btn--prev', 'discount__btn discount__btn--next']
+  });
 
-    var deactivatePromoSlide = function () {
-      promoSlides[activePromoSlideIndex].classList.remove('promo__slide--active');
-      dots[activePromoSlideIndex].classList.remove('slider__dot--active');
-    };
-
-    var activateNoveltiesSlide = function () {
-      noveltiesSlides[activeNoveltiesSlideIndex].classList.add('novelties-of-week__slide--active');
-    };
-
-    var deactivateNoveltiesSlide = function () {
-      noveltiesSlides[activeNoveltiesSlideIndex].classList.remove('novelties-of-week__slide--active');
-    };
-
-    var activateDiscountSlide = function () {
-      discountSlides[activeDiscountSlideIndex].classList.add('discount__slide--active');
-    };
-
-    var deactivateDiscountSlide = function () {
-      discountSlides[activeDiscountSlideIndex].classList.remove('discount__slide--active');
-    };
-
-
-    var nextPromoSlide = function (direction) {
-      deactivatePromoSlide();
-      switch (direction) {
-        case PREV:
-          if (--activePromoSlideIndex < 0) {
-            activePromoSlideIndex = promoSlides.length - 1;
-          }
-          break;
-        case NEXT:
-          if (++activePromoSlideIndex > promoSlides.length - 1) {
-            activePromoSlideIndex = 0;
-          }
-          break;
+  $('.brands__slides').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navContainerClass: ['slider-nav', 'slider-nav--brands'],
+    navClass: ['slider__btn slider__btn--prev', 'slider__btn slider__btn--next'],
+    responsive: {
+      768: {
+        items: 3
+      },
+      1140: {
+        items: 6
       }
-      activatePromoSlide();
-    };
+    }
+  });
 
-    var nextNoveltiesSlide = function (direction) {
-      deactivateNoveltiesSlide();
-      switch (direction) {
-        case PREV:
-          if (--activeNoveltiesSlideIndex < 0) {
-            activeNoveltiesSlideIndex = noveltiesSlides.length - 1;
-          }
-          break;
-        case NEXT:
-          if (++activeNoveltiesSlideIndex > noveltiesSlides.length - 1) {
-            activeNoveltiesSlideIndex = 0;
-          }
-          break;
+  $('.sections__slides').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navContainerClass: ['slider-nav', 'slider-nav--sections'],
+    navClass: ['slider__btn slider__btn--prev', 'slider__btn slider__btn--next'],
+    navRewind: false,
+    responsive: {
+      768: {
+        items: 3
+      },
+      1140: {
+        items: 4
       }
-      activateNoveltiesSlide();
-    };
-
-    var nextDiscountSlide = function (direction) {
-      deactivateDiscountSlide();
-      switch (direction) {
-        case PREV:
-          if (--activeDiscountSlideIndex < 0) {
-            activeDiscountSlideIndex = discountSlides.length - 1;
-          }
-          break;
-        case NEXT:
-          if (++activeDiscountSlideIndex > discountSlides.length - 1) {
-            activeDiscountSlideIndex = 0;
-          }
-          break;
-      }
-      activateDiscountSlide();
-    };
-
-    var onDotCLick = function (evt) {
-      var target = evt.target;
-
-      while (target !== dotsContainer) {
-        if (target.classList.contains('slider__dot')) {
-          deactivatePromoSlide();
-          activePromoSlideIndex = [].indexOf.call(dots, target);
-          activatePromoSlide();
-          break;
-        }
-        target = target.parentNode;
-      }
-    };
-
-    var onClickPromoPrev = function () {
-      nextPromoSlide(PREV);
-    };
-
-    var onClickPromoNext = function () {
-      nextPromoSlide(NEXT);
-    };
-
-    var onClickNoveltiesPrev = function () {
-      nextNoveltiesSlide(PREV);
-    };
-
-    var onClickNoveltiesNext = function () {
-      nextNoveltiesSlide(NEXT);
-    };
-
-    var onClickDiscountPrev = function () {
-      nextDiscountSlide(PREV);
-    };
-
-    var onClickDiscountNext = function () {
-      nextDiscountSlide(NEXT);
-    };
-
-    renderDots();
-    btnPromoPrev.addEventListener('click', onClickPromoPrev);
-    dotsContainer.addEventListener('click', onDotCLick);
-    btnPromoNext.addEventListener('click', onClickPromoNext);
-    btnNoveltiesPrev.addEventListener('click', onClickNoveltiesPrev);
-    btnNoveltiesNext.addEventListener('click', onClickNoveltiesNext);
-    btnDiscountPrev.addEventListener('click', onClickDiscountPrev);
-    btnDiscountNext.addEventListener('click', onClickDiscountNext);
-  }
-})();
+    }
+  });
+});
 
 
 'use strict';
@@ -443,41 +346,6 @@ jQuery(document).ready(function($) {
       })
     })
   })();
-});
-
-$(function() {
-  $('.brands__slides').owlCarousel({
-    items: 1,
-    loop: true,
-    nav: true,
-    navContainerClass: ['slider-nav', 'slider-nav--brands'],
-    navClass: ['slider__btn slider__btn--prev', 'slider__btn slider__btn--next'],
-    responsive: {
-      768: {
-        items: 3
-      },
-      1140: {
-        items: 6
-      }
-    }
-  });
-
-  $('.sections__slides').owlCarousel({
-    items: 1,
-    loop: true,
-    nav: true,
-    navContainerClass: ['slider-nav', 'slider-nav--sections'],
-    navClass: ['slider__btn slider__btn--prev', 'slider__btn slider__btn--next'],
-    navRewind: false,
-    responsive: {
-      768: {
-        items: 3
-      },
-      1140: {
-        items: 4
-      }
-    }
-  });
 });
 
 'use strict';
